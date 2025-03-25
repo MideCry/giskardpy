47c2300cc5bb647f44321695545fef2187b3665a
Lukas Hauschild <84103007+MideDay@users.noreply.github.com>
2025-03-25 14:32:42 +0100

    Revert "fixed WorldWithFixedRobot"
    This reverts commit c247b3fd3ae8f18a41ee096aad54ca0c965b12d3.

--- giskardpy/model/world_config.py
+++ giskardpy/model/world_config.py
@@ -14,7 +14,6 @@ from giskardpy.model.world import WorldTree
 from giskardpy.data_types.data_types import my_string, PrefixName, Derivatives, derivative_map, ColorRGBA
 import giskardpy.casadi_wrapper as cas
 
-
 class WorldConfig(ABC):
     _world: WorldTree
     _default_limits = {
@@ -215,18 +214,12 @@ class EmptyWorld(WorldConfig):
 
 
 class WorldWithFixedRobot(WorldConfig):
-    def __init__(self,
-                 urdf: str,
-                 map_name: str = 'map'):
+    def __init__(self, joint_limits: Dict[Derivatives, float] = None):
         super().__init__()
-        self.urdf = urdf
-        self.map_name = PrefixName(map_name)
+        self._joint_limits = joint_limits
 
-    def setup(self, robot_name: Optional[str] = None) -> None:
-        self.add_empty_link(self.map_name)
-        self.add_robot_urdf(self.urdf, robot_name)
-        root_link_name = self.get_root_link_of_group(self.robot_group_name)
-        self.add_fixed_joint(parent_link=self.map_name, child_link=root_link_name)
+    def setup(self, robot_description: str, robot_name: str) -> None:
+        self.add_robot_urdf(robot_description, robot_name)
 
 
 class WorldWithOmniDriveRobot(WorldConfig):
